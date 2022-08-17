c23386895a36ab603608ffbcdd2d71b53cc9d6eb
rayct <rayct@icloud.com>
2022-08-17 14:16:42 +0100

    Handling API Errors

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,24 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import {
+  createRouter,
+  createWebHistory
+} from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import CityView from "../views/CityView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
+  history: createWebHistory(
+    import.meta.env.BASE_URL),
+  routes: [{
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
-  ]
-})
+    {
+      path: "/weather/:state/:city",
+      name: "cityView",
+      component: CityView,
+    },
+  ],
+});
 
-export default router
+export default router;
