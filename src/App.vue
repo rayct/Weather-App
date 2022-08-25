06f6702b5568ee0e7bc2bbb004df8570cb1067af
rayct <rayct@icloud.com>
2022-08-26 00:21:30 +0100

    Project Completed

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="flex flex-col min-h-screen font-Poppins bg-weather-primary">
+  <div
+    class="flex flex-col min-h-screen font-Roboto bg-weather-primary"
+  >
     <SiteNavigation />
-    <RouterView />
+    <RouterView class="flex-1" v-slot="{ Component }">
+      <Transition name="page">
+        <component :is="Component" />
+      </Transition>
+    </RouterView>
   </div>
 </template>
 
@@ -10,5 +16,11 @@ import { RouterView } from "vue-router";
 import SiteNavigation from "./components/SiteNavigation.vue";
 </script>
 
-<style lang="scss" scoped>
+<style>
+.page-enter-active {
+  transition: 600ms ease all;
+}
+.page-enter-from {
+  opacity: 0;
+}
 </style>
--- src/views/CityView.vue
+++ src/views/CityView.vue
@@ -1,14 +1,17 @@
 <template>
-    <div>
-        <Suspense>
-            <AsyncCityView />
-            <template #fallback>
-                <p>Loading...</p>
-            </template>
-        </Suspense>
-    </div>
+  <div>
+    <Suspense>
+      <template #default>
+        <AsyncCityView />
+      </template>
+      <template #fallback>
+        <CityViewSkeleton />
+      </template>
+    </Suspense>
+  </div>
 </template>
 
 <script setup>
 import AsyncCityView from "../components/AsyncCityView.vue";
+import CityViewSkeleton from "../components/CityViewSkeleton.vue";
 </script>
